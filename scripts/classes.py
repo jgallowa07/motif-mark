bb17082a17eee66b8ded28922bed003f2c73ee8a
jgallowa07 <jaredgalloway07@gmail.com>
2020-03-09 16:17:41 -0700

    finished all drawing

--- scripts/classes.py
+++ scripts/classes.py
@@ -14,6 +14,7 @@ import sys
 import re
 import numpy as np
 import numpy as np
+import cairo
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
@@ -27,8 +28,7 @@ class MotifController(object):
     Along with storing patterns and colors
     """
 
-
-    def __init__(self, motif_file_path):
+    def __init__(self, motif_file_path, cmap):
 
         self.motifs = []
         self.colors = []
@@ -38,15 +38,13 @@ class MotifController(object):
         fp = open(motif_file_path)
         for i,line in enumerate(fp):
             motif = line.strip()
-            # TODO Error checking here.
             self.motifs.append(motif)
             self.patterns.append(self._create_regex_pattern_from_motif(motif))
 
         self.num_motifs = i+1
-        color_map = cm.get_cmap('viridis', self.num_motifs)
+        color_map = cm.get_cmap(cmap, self.num_motifs)
         self.colors = color_map.colors
         
-
     def _create_regex_pattern_from_motif(self, motif):
         """
         string -> string
@@ -55,18 +53,15 @@ class MotifController(object):
         pattern to account for sequence ambiguity.
         """
 
-        # TODO question: how do we deal with U?
         pattern_dict = {
-            "U":"T", "u":"t",
-            "Y":"[CT]", "R":"[AG]", "W":"[AT]", "S":"[GC]", "K":"[TG]", "M":"[CA]",
-            "D":"[AGT]", "V":"[ACG]", "H":"[ACT]", "B":"[CGT]", "N":"[ATCG]", "X":"[ATCG]",
+            "u":"[ut]","t":"[ut]",
             "y":"[ct]", "r":"[ag]", "w":"[at]", "s":"[gc]", "k":"[tg]", "m":"[ca]",
             "d":"[agt]", "v":"[acg]", "h":"[act]", "b":"[cgt]", "n":"[atcg]", "x":"[atcg]"
             }
         
         pattern = ""
-        for base in motif:
-            if base in 'atcgATCG':
+        for base in motif.lower():
+            if base in 'atcg':
                 pattern += base
             else:
                 pattern += pattern_dict[base]
@@ -102,15 +97,13 @@ class SequenceInformation(object):
         motif_controller
         ):
 
-        self.length = len(sequence)
-        self.start_position = None
-        self.end_position = None
+        self.header = header 
+        self.num_nuc = len(sequence)
         self.exon_start = None
         self.exon_end = None
         self.motifs = []
 
         self._extract_motif_information(sequence, motif_controller)
-        # TODO start/end position from header
 
     def _extract_motif_information(self, sequence, mot_con):
         """
@@ -131,17 +124,17 @@ class SequenceInformation(object):
         exon_flag = False
         for motif_idx, (pattern, motif) in enumerate(zip(mot_con.patterns,mot_con.motifs)):
             window_size = len(motif)
-            for seq_idx in range(self.length - window_size + 1):
+            for seq_idx in range(self.num_nuc - window_size + 1):
 
                 # TODO question : is there only one exon per seq?
                 if str.isupper(sequence[seq_idx]) and not exon_flag:
                     self.exon_start = seq_idx
                     exon_flag = True
-                if exon_flag and str.islower(sequence[seq_idx]):
+                if str.islower(sequence[seq_idx]) and exon_flag:
                     self.exon_end = seq_idx
                     exon_flag = False
                 
-                match = re.match(pattern, sequence[seq_idx:seq_idx + window_size])
+                match = re.match(pattern, sequence[seq_idx:seq_idx + window_size].lower())
                 if match == None:
                     continue
                 else:
@@ -158,16 +151,131 @@ class SequenceInformation(object):
 class SequenceDrawer(object):
     """
     This class will handle drawing sequences
-    given 
+    given all sequence data objects in a list,
+    a respective motif controller,
+    and an output pdf filepath
     """
 
-    pass
 
+    def __init__(self, motif_controller, sequence_info_list, output):
+        # feed a list of sequence info object, and the respective 
+        # motif controller to draw sequences.
+        self.spread = 128
+        self.top_space = 128
 
+        self.motif_controller = motif_controller
+        self.sequence_info_list = sequence_info_list
+        self.output = output
+        self.width = 1024
+        self.height = self.spread * len(sequence_info_list) + self.top_space
+        self.surface = cairo.PDFSurface(self.output, self.width, self.height)        
+        self.ctx = cairo.Context(self.surface)
+        self.ctx.scale(self.width, self.height)
+        self.ctx.rectangle(0, 0, 1, 1)  # Rectangle(x0, y0, x1, y1)
+        self.ctx.set_source_rgb(1, 1, 1)
+        self.ctx.fill()
 
+        self.draw_all_sequences()
 
+    def draw_all_sequences(self):
+        
+        
+        # set some constants
+        right_margin = 0.2
+        left_margin = 0.1
+        motif_height = 0.01
+        exon_height = 0.025
+        header_margins = 40
+        legend_margins = 20
+        box_text_margin = 10
 
+        longest = max([seq_data.num_nuc for seq_data in self.sequence_info_list])
 
+        # make legend first
+        top_left_x = 1 - right_margin + (legend_margins / self.height)
+        top_left_y = (legend_margins / self.height)
+        
+        legend_width = (1 - legend_margins / self.width) - top_left_x
+        legend_height = len(self.sequence_info_list) * (legend_margins / self.height)
+        
+        self.ctx.rectangle(top_left_x, top_left_y, legend_width, legend_height)
+        self.ctx.set_source_rgba(0, 0, 0, 0.9)
+        self.ctx.set_line_width(0.001)
+        self.ctx.stroke()
 
+        for i in range(self.motif_controller.num_motifs):
+
+            y = top_left_y + (i * (legend_margins / self.height)) + ((legend_margins / self.height) / 2)
+            width = box_text_margin / self.width
+
+            self.ctx.rectangle(top_left_x + (legend_margins / self.width), y, width, width)
+            r = self.motif_controller.colors[i][0]
+            g = self.motif_controller.colors[i][1]
+            b = self.motif_controller.colors[i][2]
+            self.ctx.set_source_rgba(r, g, b, 0.7)
+            self.ctx.fill()
+            
+            self.ctx.set_source_rgb(0, 0, 0)            
+            self.ctx.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+            self.ctx.set_font_size(0.01)
+            self.ctx.move_to((2*width) + top_left_x + (legend_margins / self.width), y)
+            self.ctx.show_text(self.motif_controller.motifs[i])
+
+        # draw each sequence one at a time
+        for i,seq_info in enumerate(self.sequence_info_list):
+
+            # where is the "center" height for each sequence
+            y = ((i * self.spread) + (self.spread // 2) + self.top_space) / self.height
+            
+            # draw header
+            self.ctx.set_source_rgb(0, 0, 0)            
+            self.ctx.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+            self.ctx.set_font_size(0.015)
+            self.ctx.move_to(left_margin, y - (header_margins/self.height))
+            self.ctx.show_text(seq_info.header)
+            
+            # draw sequence line
+            right_x = seq_info.num_nuc / longest - right_margin
+            total_length = right_x - left_margin
+
+            self.ctx.move_to(left_margin, y)
+            self.ctx.line_to(right_x, y)
+            self.ctx.set_source_rgb(0, 0, 0)
+            self.ctx.set_line_width(0.005)
+            self.ctx.stroke()
+
+            # draw exon
+            start_prop = seq_info.exon_start / seq_info.num_nuc
+            end_prop = seq_info.exon_end / seq_info.num_nuc
+            
+            top_left_x = (start_prop * total_length) + left_margin
+            top_left_y = y - exon_height
+            width = ((end_prop * total_length) + left_margin) - top_left_x
+            height = exon_height * 2
+            
+            self.ctx.rectangle(top_left_x, top_left_y, width, height)
+            self.ctx.set_source_rgba(0, 0, 0, 0.9)
+            self.ctx.set_line_width(0.001)
+            self.ctx.stroke()
+                    
+            # draw each motif
+            for motif in seq_info.motifs:
+                
+                start_prop = motif.start_position / seq_info.num_nuc
+                end_prop = motif.end_position / seq_info.num_nuc
+                
+                top_left_x = (start_prop * total_length) + left_margin
+                top_left_y = y - motif_height
+                width = ((end_prop * total_length) + left_margin) - top_left_x
+                height = motif_height * 2
+                
+                self.ctx.rectangle(top_left_x, top_left_y, width, height)
+                r = self.motif_controller.colors[motif.motif_index][0]
+                g = self.motif_controller.colors[motif.motif_index][1]
+                b = self.motif_controller.colors[motif.motif_index][2]
+                self.ctx.set_source_rgba(r, g, b, 0.7)
+                self.ctx.fill()
+
+        return None
 
 
--- scripts/motif_mark.py
+++ scripts/motif_mark.py
@@ -2,61 +2,42 @@
 import argparse
 from collections import defaultdict
 import sys
-#import gzip
 import numpy as np
 from helpers import *
 from classes import *
 
-# TODO This is a rough draft.
-parser = argparse.ArgumentParser(description='This \
-    program will take as input a fasta file, \
-    and a file with motif sequences of interest found in transcripts. \
-    and produce a plot with the exons and motifs labled. \
-    A subplot containing a linear cartoon will be produced in one pdf \
-    where each sequence in the fasta gets a single line.')
-
-parser.add_argument('-fasta', type=str,
-    help='fasta file containing transcripts where we are looking for motifs \
-    This file can be ')
-
-parser.add_argument('-motifs', type=str, 
-    help=' properly formatted \
-    list of valid motifs to be labled on cartoon. \
-    This should be a text file where each motif is seperated \
-    by a new line')
-
-parser.add_argument('-out', type=str, 
-    help='The name of the output cartoon')
-
-# TODO
-# could add color palette
-# could add output pref, svg of pdf.
-
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='This \
+        program will take as input a fasta file, \
+        and a file with motif sequences of interest found in transcripts. \
+        and produce a plot with the exons and motifs labled. \
+        A subplot containing a linear cartoon will be produced in one pdf \
+        where each sequence in the fasta gets a single line.')
+
+    parser.add_argument('-fasta', type=str,
+        help='fasta file containing transcripts where we are looking for motifs \
+        This file can be ')
+
+    parser.add_argument('-motifs', type=str, 
+        help=' properly formatted \
+        list of valid motifs to be labled on cartoon. \
+        This should be a text file where each motif is seperated \
+        by a new line')
+
+    parser.add_argument('-cmap', type=str, default='plasma', \
+        help=' A color map of your choosing. The default is \
+        plasma, but viridis, magma, inferno, and cividis are all \
+        acceptable inputs')
+
+    parser.add_argument('-out', type=str, default='motif_mark.pdf', \
+        help='The name of the output pdf cartoon, default is \
+        motif_mark.pdf')
     
     args = parser.parse_args()
     
-    motif_controller = MotifController(args.motifs)
-    print('\n'.join(motif_controller.patterns))
-    #for color in motif_controller.colors:
-    #    print(color)
-    sequence_iterator =  fasta_record_iter(open(args.fasta))
-    
-    first = next(sequence_iterator)
-    print('\n'.join(first))
-
-    seq_info = SequenceInformation(first[0],first[1],motif_controller)
-    #print(f"motif: {seq_info.motifs}")
-    for motif in seq_info.motifs:
-        print(f"start: {motif.start_position}, end: {motif.end_position}, m_idx: \
-{motif.motif_index}, sequence: {first[1][motif.start_position:motif.end_position]}")
-    print(f"exon start: {seq_info.exon_start}, exon_end: {seq_info.exon_end}")
-
-    
-
-
-    
-
-    
-        
+    motif_controller = MotifController(args.motifs, args.cmap)
+    sequence_iterator = fasta_record_iter(open(args.fasta))
+    sequence_info_list = [SequenceInformation(seq[0],seq[1],motif_controller) for seq in sequence_iterator]
+    SequenceDrawer(motif_controller, sequence_info_list, args.out)
